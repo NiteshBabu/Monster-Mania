0fe618cd1d9603efac8c9d467387072d66b6b7db
NiteshBabu <niteshbabusharma@gmail.com>
2020-08-10 17:08:23 +0530

    Updates

---
+++ precache-manifest.2adf298d5681411096a506da411acc3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5198d342c99a9bf0450e9b469992af99",
+    "url": "/Monster-Mania/index.html"
+  },
+  {
+    "revision": "1984819d1421a87f1b4a",
+    "url": "/Monster-Mania/static/css/main.4d41bc98.chunk.css"
+  },
+  {
+    "revision": "e4a6acb6a9f7df76e39c",
+    "url": "/Monster-Mania/static/js/2.1d64572c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monster-Mania/static/js/2.1d64572c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1984819d1421a87f1b4a",
+    "url": "/Monster-Mania/static/js/main.0c11c588.chunk.js"
+  },
+  {
+    "revision": "0d6ddccdb3a1cb3e8ff0",
+    "url": "/Monster-Mania/static/js/runtime-main.2bb5a888.js"
+  }
+]);
